fe76651a9da505f167733d8799dcf9e6e59113b3
chunnit99 <kienchun209@gmail.com>
2020-11-20 22:14:10 +0700

    first commit

---
+++ BTLWeb/src/test/java/com/example/BTLWeb/BtlWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BTLWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
